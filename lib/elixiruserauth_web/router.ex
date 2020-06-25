549103ad52c59f4f78861b9dada26adf33f769b9
Christian Deacon <gamemann@gflclan.com>
2020-06-25 00:33:30 +0000

    Updated routes.

--- lib/elixiruserauth_web/router.ex
+++ lib/elixiruserauth_web/router.ex
@@ -14,7 +14,17 @@ defmodule ElixiruserauthWeb.Router do
   end
 
   scope "/", ElixiruserauthWeb do
-    pipe_through :browser
+    pipe_through [:browser, ElixiruserauthWeb.Plugs.NotUser]
+
+    resources "/users", UserController, only: [:create, :new, :edit]
+    get "login", SessionController, :new
+    post "login", SessionController, :create
+  end
+
+  scope "/", ElixiruserauthWeb do
+    pipe_through [:browser, ElixiruserauthWeb.Plugs.User]
+
+    delete "/logout", SessionController, :delete
 
     get "/", PageController, :index
   end
