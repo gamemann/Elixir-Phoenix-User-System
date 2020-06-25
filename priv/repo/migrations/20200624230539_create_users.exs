1f4868a327ab3732db4153a10affdc29927e2ea2
Christian Deacon <gamemann@gflclan.com>
2020-06-25 02:10:39 +0000

    Added users migration.

---
+++ priv/repo/migrations/20200624230539_create_users.exs
@@ -0,0 +1,14 @@
+defmodule Elixiruserauth.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string
+      add :epassword, :string
+      add :email, :string
+
+      timestamps()
+    end
+
+  end
+end
